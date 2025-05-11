f2984f1452f233d0f6589a4d715827c2b9164fd4
ArifBabayev05 <arifrb@code.edu.az>
2025-05-11 20:49:48 +0400

    Stop tracking JSON database files

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,6 @@
-import time
-import sys
-import os
 from bot import main
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Bot is running!".encode()]
+application = main()
 
-# Run the bot in the background
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        print(f"Bot crashed: {e}")
+    application.run()
